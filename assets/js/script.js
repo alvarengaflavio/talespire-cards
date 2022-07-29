c80acf776161bb62c3e4d799a7d2a052e7f7242e
alvalenda <banysan@msn.com>
2022-07-29 17:50:07 -0300

    +js onethird event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
+const third = document.querySelectorAll('.one-third');
+const noborder = document.querySelector('.no-border');
 
+third.forEach(element => {
+    element.addEventListener('mouseover', () => {
+        element.style.boxShadow = '-1px -1px 2px 2px #bd7c2f';
+    });
+
+    element.addEventListener('mouseout', () => {
+        element.style.boxShadow = 'none';
+    });
+});
